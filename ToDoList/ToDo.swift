54ee90250c1e69f46283ff7b4ab9983709e6906e
lindaweng <lindaweng4@gmail.com>
2020-07-27 22:33:58 -0400

    Complete Iteration 0

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Linda Weng on 7/27/20.
+//  Copyright © 2020 Linda Weng. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
